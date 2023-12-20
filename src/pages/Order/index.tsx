d8c29061e50427bbe90fe8221e52aa2a50b6d4d8
Manoela Moreno Frattini <manoelamoreno78@gmail.com>
2023-12-20 14:42:31 -0300

    sair

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -90,7 +90,7 @@ export default function Order() {
   const navigate = useNavigate();
   const handleBack = () => {
     console.log("Voltando...");
-    navigate("/login");
+    navigate("/");
   };
 
   return (
